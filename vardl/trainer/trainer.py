a1bdc2b32a778b4ebe7973d9219e1e0d20c12b6a
Simone <simone.rossi.93@gmail.com>
2018-09-03 10:39:28 +0200

    [add] Add train/test handler for trainer

--- vardl/trainer/trainer.py
+++ vardl/trainer/trainer.py
@@ -23,7 +23,7 @@ from typing import Dict
 from termcolor import colored
 
 from ..utils import set_seed
-
+from ..logger import BaseLogger
 
 class TrainerRegressor():
 
@@ -34,7 +34,8 @@ class TrainerRegressor():
                  train_dataloader: DataLoader,
                  test_dataloader: DataLoader,
                  device: str,
-                 seed: int):
+                 seed: int,
+                 logger: BaseLogger=None):
         assert device == 'cuda' or device == 'cpu'
         assert optimizer == 'Adam'
 
@@ -50,83 +51,94 @@ class TrainerRegressor():
         self.train_dataloader = train_dataloader
         self.test_dataloader = test_dataloader
 
+        self.logger = logger
+
         set_seed(seed)
 
-    def compute_nell(self, Y_pred: torch.Tensor, Y_true: torch.Tensor, n: int, m: int):
+    def compute_nell(self, Y_pred: torch.Tensor, Y_true: torch.Tensor, n: int, m: int) -> torch.Tensor:
         nell = - n / m * torch.sum(torch.mean(self.model.likelihood.log_cond_prob(Y_true, Y_pred), 0))
         return nell
 
-    def compute_loss(self, Y_pred: torch.Tensor, Y_true: torch.Tensor, n: int, m: int):
+    def compute_loss(self, Y_pred: torch.Tensor, Y_true: torch.Tensor, n: int, m: int) -> torch.Tensor:
         return self.compute_nell(Y_pred, Y_true, n, m) + self.model.dkl
 
-    def compute_error(self, Y_pred, Y_true):
+    def compute_error(self, Y_pred: torch.Tensor, Y_true: torch.Tensor) -> torch.Tensor:
         return torch.sqrt(torch.mean(torch.pow((Y_true - Y_pred), 2)))  # ok for regression
 
 
+    def train_batch(self, data: torch.Tensor, target: torch.Tensor,
+                    train_verbose: bool, train_log_interval: int):
 
-    def train_batch(self, data, target):
-        """ Implement the logic of training with one batch. """
         self.current_iteration += 1
 
         data, target = data.to(self.device), target.to(self.device)
         self.optimizer.zero_grad()
         output = self.model(data)
         loss = self.compute_loss(output, target, len(self.train_dataloader.dataset), data.size(0))
+        error= self.compute_error(output, target),
         loss.backward()
 
-        train_log_interval = 100
-        if self.current_iteration % train_log_interval == 0:
+
+        if self.current_iteration % train_log_interval == 0 and train_verbose:
             print(colored('Train', 'blue', attrs=['bold']),
                   "|| iter=%5d   loss=%10.0f  error=%.2f  log_theta_noise_var=%5.2f" %
-                  (self.current_iteration, loss.item(), self.compute_error(output, target),  self.model.likelihood.log_noise_var.item()))
+                  (self.current_iteration, loss.item(), error.item(),
+                   self.model.likelihood.log_noise_var.item()))
+
+        self.logger.scalar_summary('loss/train', loss, self.current_iteration)
+
         self.optimizer.step()
 
-        #print(loss)
-        #print('Batch done')
 
-    def train(self):
+    def train_per_iterations(self, iterations: int, train_verbose: bool, train_log_interval: int):
         """ Implement the logic of training the model. """
         self.model.train()
-        for _ in range(self.config.epoch):
-            self.cur_epoch += 1
-            self.train_epoch()
-            self.test()
+        dataloader_iterator = iter(self.train_dataloader)
+        self.current_epoch += 1
 
-    def train_epoch(self):
-        """ Implement the logic of training one epoch. """
-        for batch_idx, (data, target) in tqdm(
-                enumerate(self.data_loader.train_loader)):
-            self.train_batch(batch_idx, data, target)
+        for i in range(iterations):
+            try:
+                data, target = next(dataloader_iterator)
+            except:
+                del dataloader_iterator
+                dataloader_iterator = iter(self.train_dataloader)
+                data, target = next(dataloader_iterator)
+                self.current_epoch += 1
+
+            self.train_batch(data, target, train_verbose, train_log_interval)
+
+
+    def train_epochs(self, epochs: int):
+
+        for _ in range(epochs):
+            self.current_epoch += 1
+            for batch_idx, (data, target) in enumerate(self.train_dataloader):
+             self.train_batch(data, target)
 
 
 
     def test(self):
-        """ Implement the logic of evaluating the test set performance. """
         self.model.eval()
-        test_loss = 0
-        correct = 0
+        test_nell = 0
+        test_error = 0
         with torch.no_grad():
-            for data, target in self.data_loader.test_loader:
-                batch_loss, batch_correct = self.test_batch(data, target)
-                test_loss += batch_loss
-                correct += batch_correct
+            for data, target in self.test_dataloader:
+                data, target = data.to(self.device), target.to(self.device)
+                output = self.model(data)
+                batch_loss = self.compute_nell(output, target, len(self.test_dataloader.dataset), data.size(0))
+                test_nell += batch_loss
+                test_error += self.compute_error(output, target)
 
-        test_loss /= len(self.data_loader.test_loader.dataset)
+                test_nell /= len(self.test_dataloader.dataset)
+        test_error /= len(self.test_dataloader)
 
-        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.
-              format(
-                  test_loss, correct, len(
-                      self.data_loader.test_loader.dataset),
-                  100. * correct / len(self.data_loader.test_loader.dataset)))
+        print(colored('Test', 'green', attrs=['bold']),
+              " || iter=%5d   mnll=%10.3f   rmse=%8.3f" % (self.current_iteration, test_nell.item(), test_error.item()))
 
-    def test_batch(self, data, target):
-        """ Implement the logic of testing with one batch. """
-        data, target = data.to(self.device), target.to(self.device)
-        output = self.model(data)
-        batch_loss = F.nll_loss(
-            output, target, size_average=False).item()  # sum up batch loss
-        pred = output.max(
-            1, keepdim=True)[1]  # get the index of the max log-probability
-        batch_correct = pred.eq(target.view_as(pred)).sum().item()
 
-        return batch_loss, batch_correct
+
+    def fit(self, iterations: int, test_interval: int, train_verbose: bool, train_log_interval: int):
+        for _ in range(iterations // test_interval):
+            self.test()
+            self.train_per_iterations(test_interval, train_verbose, train_log_interval)
+        self.test()
