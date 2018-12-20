[fix] Add bernoulli distribution + kl

vardl/distributions/base_distribution.py
@@ -30,4 +30,7 @@ class BaseDistribution(nn.Module):
         NotImplementedError("Subclass should implement this.")
 
     def optimize(self, train: bool = True):
-        NotImplementedError("Subclass should implement this.")
+        for param in self.parameters():
+            param.requires_grad = train
+    #def optimize(self, train: bool = True):
+    #    NotImplementedError("Subclass should implement this.")

vardl/distributions/matrix_gaussian_distribution.py
@@ -81,7 +81,7 @@ class MatrixGaussianDistribution(BaseDistribution):
         self._logvars = nn.Parameter(
             torch.ones(self.n, self.m,   #
                        dtype=dtype,
-                       device=self.device) * np.log(1. / self.n),
+                       device=self.device) * np.log(2. / (self.n + self.m)),
             requires_grad=False)
 
         if self.approx == 'full':

vardl/distributions/multivariate_gaussian_distribution.py
@@ -75,7 +75,7 @@ class MultivariateGaussianDistribution(BaseDistribution):
         self._logvars = nn.Parameter(
             torch.ones(self.n,    #
                        dtype=dtype,
-                       device=self.device) * np.log(1. / self.n),
+                       device=self.device) * np.log(1. / (self.n + 1)),
             requires_grad=False)
 
         if self.approx == 'full':